96e25c6266e36f531cef5292785cb85281afec9a
MITATED <mitated@gmail.com>
2018-04-06 01:05:36 +0000

    End

--- rm.py
+++ rm.py
@@ -9,10 +9,13 @@ PASSWORD = "freelance"
 import sys
 from time import sleep, strftime
 
-
-
+f = open('RealMadrid.log', 'a')
+f.write("%s %s\n"%(strftime("%d.%m.%Y"), strftime("%H:%M:%S")))
+f.close()
+f = open('RealMadrid.log', 'a')
+sys.stderr = f
 ##################------Основные
-# import os
+import os
 # from os.path import split, splitext, join, exists, basename
 # from os import listdir
 # import shutil
@@ -37,6 +40,7 @@ import random
 
 ##################------selenium
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 # from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 # from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
@@ -45,181 +49,109 @@ from selenium import webdriver
 # import pyautogui
 # from pyautogui import keyDown, keyUp
 
+##################------Interface
+from tkinter import *
+
+#################-------Selenide(MITATED modyfycation)
+from selenide import Driver
 
 def UA():#Список User-Agent'ов
 	with open("UAs.txt", "r") as file:
 		lUA = file.read().split("\n")[:-1]
 	return random.choice(lUA)
 
-class Driver:#Работа с драйвером
-	"""docstring for Driver"""
-	def __init__(self, typeD = "Chrome", proxy = None, isTor = 0, UserAgent = 0):
-		# super(Driver, self).__init__()
-		self.typeD = typeD
-		self.wait = 15
-		self.isTor = isTor
-		self.UserAgent = UserAgent
-		self.proxy = proxy
-		self.preference()
-	def preference(self):
-		if self.typeD == "Chrome":
-			self.chrome_options = webdriver.ChromeOptions()
-			if self.UserAgent:
-				self.chrome_options.add_argument('--user-agent=%s'%self.UserAgent)
-			if self.isTor:
-				self.chrome_options.add_argument('--proxy-server=socks5://127.0.0.1:9150')
-			if self.proxy:
-				self.chrome_options.add_argument('--proxy-server=%s'%self.proxy)# "(http|socks4|socks5)://000.000.000.000:8080"
-		elif self.typeD == "FF":
-			self.ff_prof=webdriver.FirefoxProfile()
-			if self.UserAgent:
-				self.ff_prof.set_preference('general.useragent.override', self.UserAgent)
-			if self.proxy:
-				self.ff_prof.set_preference( "network.proxy.type", 1 )
-				self.ff_prof.set_preference( "network.proxy.socks_version", 5 )
-				self.ff_prof.set_preference( "network.proxy.socks", self.proxy.split(":")[0])
-				self.ff_prof.set_preference( "network.proxy.socks_port", int(self.proxy.split(":")[1]) )
-				self.ff_prof.set_preference( "network.proxy.socks_remote_dns", True )
-			if self.isTor:
-				tor_path = "%sTor Browser\\Browser\\firefox.exe"%dic["pathTor"]
-				# Настройки приватности
-				self.ff_prof.set_preference( "places.history.enabled", True )
-				self.ff_prof.set_preference( "privacy.clearOnShutdown.offlineApps", True )
-				self.ff_prof.set_preference( "privacy.clearOnShutdown.passwords", True )
-				self.ff_prof.set_preference( "privacy.clearOnShutdown.siteSettings", True )
-				self.ff_prof.set_preference( "privacy.sanitize.sanitizeOnShutdown", True )
-				self.ff_prof.set_preference( "signon.rememberSignons", False )
-				self.ff_prof.set_preference( "network.cookie.lifetimePolicy", 2 )
-				self.ff_prof.set_preference( "network.dns.disablePrefetch", False )
-				self.ff_prof.set_preference( "network.http.sendRefererHeader", 2 )		
-				# Настраиваем Прокси
-				self.ff_prof.set_preference( "network.proxy.type", 1 )
-				self.ff_prof.set_preference( "network.proxy.socks_version", 5 )
-				self.ff_prof.set_preference( "network.proxy.socks", '127.0.0.1' )
-				self.ff_prof.set_preference( "network.proxy.socks_port", 9150 )
-				self.ff_prof.set_preference( "network.proxy.socks_remote_dns", True )
-				# Отключаем загрузку изображений через Tor - для ускорения соединений
-				self.ff_prof.set_preference('general.useragent.override', self.UA())
-				self.binary = FirefoxBinary(tor_path)
-	def open(self):
-		if self.typeD == "Chrome":
-			self.driver = webdriver.Chrome(executable_path="chromedriver.exe",chrome_options=self.chrome_options)
-		elif self.typeD == "FF":
-			if not self.isTor:
-				self.driver = webdriver.Firefox()
-			else:
-				self.driver = webdriver.Firefox(self.ff_prof, self.binary)
-		return self.driver
-	def close(self):
-		self.driver.quit()
-	def click(self, elem):
-		try:
-			try:
-				elem.click()
-			except:
-				self.driver.execute_script("arguments[0].click(); ", elem)
-			return 1
-		except:
+
+class Run:
+	def __init__(self):
+		root=Tk()
+		label = Label(text=u"Сколько мест искать?")
+		label.grid(row=0, column=0, columnspan=7)
+		button1 = Button(command=lambda: self.real(1), text=u"1")
+		button1.grid(row=1, column=1, ipadx=10, ipady=2)
+		button2 = Button(command=lambda: self.real(2), text=u"2")
+		button2.grid(row=1, column=2, ipadx=10, ipady=2)
+		button3 = Button(command=lambda: self.real(3), text=u"3")
+		button3.grid(row=1, column=3, ipadx=10, ipady=2)
+		button4 = Button(command=lambda: self.real(4), text=u"4")
+		button4.grid(row=1, column=4, ipadx=10, ipady=2)
+		root.mainloop()
+
+	def real(self, NUM):
+		ua = UA() #Read useragents
+		driver = Driver(UserAgent = ua, typeD = "Chrome") #Initialize driver/browser
+
+		driver.open()#Open driver/browser
+		print("Maximize and fixed current window")
+		driver.max_win() #For full monitor
+
+		self.goto(driver, NUM)
+
+
+	def goto(self, driver, NUM):
+		window = driver.driver.current_window_handle
+
+		print("GoTo")
+		driver.get("https://www.realmadrid.com/en/tickets")
+
+		if not driver.xpath("//*[contains(@class, 'header_section')]", is_click=0):
+			print("Not tickets-page")
 			return 0
-	def xpath(self, xpath, is_one = 1, is_displayed = 1, is_click = 1, clickAll = 0, start_wait = 1.5, wait = None):
-		sleep(start_wait)
-		thisWait = wait if wait != None else self.wait
-		for waitOne in range(thisWait + 1):
-			elems = self.driver.find_elements_by_xpath(xpath)
-			if is_one and len(elems) >= 1 and ((not is_displayed) or (is_displayed and elems[0].is_displayed())):
-				if is_click:
-					return self.click(elems[0]), elems[0]
-				else:
-					return elems[0]
-			elif not is_one and len(elems) >= 1:
-				retElems = []
-				for elem in elems:
-					if is_displayed and not elem.is_displayed():
+
+
+		print("Click full button")
+		driver.xpath("//a[@class='btn btn_full']")
+
+		print("Switch window")
+		driver.driver.switch_to_window(window)
+		driver.driver.close()
+		windows = driver.driver.window_handles
+		driver.driver.switch_to_window(windows[0])
+		self.sel(driver, NUM)
+		driver.close()
+
+
+	def sel(self, driver, NUM, errs = []):
+		if not driver.xpath("//*[@id='selectNumEntradas']", is_click=0):
+			print("Not Number of tickets")
+			return 0
+
+
+		select = Select(driver.xpath('//select[@id="num-entradas"]', is_click = 0, start_wait = 2.5, wait = 5))
+		select.select_by_value("%s" % NUM)
+		driver.xpath("//*[@id='boton-seleccion-entradas']")
+
+		if not driver.xpath("//*[@id='sectors-list']", is_click=0):
+			print("Not SEAT SELECTION")
+			return 0
+
+		sectors = ("padredamian", "conchaespina", "castellana", "rafaelsalgado") #Sectors
+		for sector in sectors:
+			driver.xpath("//span[@data-sector='%s']" % sector)
+			x = driver.xpath("//*[@data-available-seats][@data-zone-with-promo]", is_one=0, is_click=0)
+			for i in x:
+				if int(i.get_attribute("data-available-seats")) >= NUM:
+					print("click1")
+					id = i.get_attribute("id")
+					if id in errs:
 						continue
-					if clickAll:
-						x = self.click(elem)
-					retElems += [elem]
-				return retElems
-			sleep(1)
-		return [0, 0] if not is_one else []
-	def max_win(self):#Размер браузера - на все окно
-		self.driver.maximize_window()
-	def get(self, path):
-		self.driver.get(path)
-	def js(self, script = None, path = None):
-		if script:
-			self.driver.execute_script(script)
-		elif path:
-			with open(path, "r") as file:
-				self.driver.execute_script(file.read())
+					driver.xpath("//*[@id='%s_t']" % id)
+					if driver.xpath("//*[@id='message-alert']"):
+						driver.xpath("//*[@id='alert-ok']")
+						errs += [id]
+						self.sel(driver, NUM)
+					break
+			else:
+				driver.xpath("//a[@id='boton-compra-back']")
+				continue
+			break
 		else:
-			print("ERROR: Not js for running!!!")
-
-###################################################
-
-
-
-
-try:# Настройки
-	with open("settings.ini", "r") as file:
-		dic = {}
-		for i in file.read().split("\n")[:-1]:
-			dic[i.split(":::")[0]] = i.split(":::")[1]
-except:
-	pass
-
-NUM = 4
-
-from selenium.webdriver.support.ui import Select
-print( "TEST")
-
-ua = UA()
-driver = Driver(UserAgent = ua, typeD = "Chrome")
-print(1)
-
-driver.open()
-print("Maximize and fixed current window")
-driver.max_win()
-window = driver.driver.current_window_handle
-
-print("GoTo")
-driver.get("https://www.realmadrid.com/en/tickets")
-
-print("Click full button")
-driver.xpath("//a[@class='btn btn_full']")
-
-print("Switch window")
-driver.driver.switch_to_window(window)
-driver.driver.close()
-windows = driver.driver.window_handles
-driver.driver.switch_to_window(windows[0])
+			self.goto(driver, NUM)
+		if driver.xpath("//*[@id='boton-compra']"):
+			print("Congratulations")
+			os.system("beep.mp3")
 
 
-# padredamian
-# conchaespina
-# castellana
-# rafaelsalgado
 
-select = Select(driver.xpath('//select[@id="num-entradas"]', is_click = 0, start_wait = 2.5, wait = 5))
-
-select.select_by_value("%s" % NUM)
+Run()
 
 
-driver.xpath("//*[@id='boton-seleccion-entradas']")
-
-
-driver.xpath("//span[@data-sector='rafaelsalgado']")
-
-
-for i in range(NUM, 15):
-	if driver.xpath("//*[@data-available-seats='%s']" % i, is_one=1, is_click=1):
-		break
-
-
-sleep(500)
-
-driver.close()
-
-################################################
-print( "end(5)")
-sleep(5)
